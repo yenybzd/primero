8084db856c6078853dc82b11e190a11d7a82be83
yeny <yenybzd@hotmail.com>
2014-11-09 10:04:07 -0600

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiblobApp::Application.config.secret_key_base = '27b6d575538bfe4fefd31672d28c326b0ea8a8aac21759c0bb4ce23800b2114ac9d2049e3092a42cd06d5231f86c0ae7c5070238c3a7af93152eb51d7513dc53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiblobApp::Application.config.session_store :cookie_store, key: '_miblob_app_session'
